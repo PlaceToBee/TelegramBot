4cdb229380d94c2a3f565c6432cab1781fbd9547
Артур <83964423+PlaceToBee@users.noreply.github.com>
2021-08-18 10:47:25 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '1906022557:AAFaSqUVrmmphSqbnyPfqcf-eMhfoq6oyuI'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'рипл': 'XRP',
+}
